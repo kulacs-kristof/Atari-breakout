4e352ec2cc8e5bdaf56bce48fce8e8d7fba0f78a
kulacs-kristof <kulacs.kristof@students.jedlik.eu>
2024-06-02 18:45:08 +0200

    teszt

---
+++ FlappyBirdTest/UnitTest1.cs
@@ -0,0 +1,33 @@
+using System;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System.Threading;
+
+namespace FlappyBirdTests
+{
+    public class FlappyBirdGameTests
+    {
+        private IWebDriver _driver;
+        private IJavaScriptExecutor _js;
+
+        [SetUp]
+        public void Setup()
+        {
+            var chromeOptions = new ChromeOptions();
+            chromeOptions.AddArgument("--disable-infobars");
+            chromeOptions.AddArgument("--disable-extensions");
+            chromeOptions.AddArgument("--headless"); // Run headless for testing purposes
+            _driver = new ChromeDriver(chromeOptions);
+            _js = (IJavaScriptExecutor)_driver;
+            _driver.Navigate().GoToUrl("../../game.html");  // Adjust the path accordingly
+            _driver.Manage().Timeouts().PageLoad = TimeSpan.FromSeconds(10);
+        }
+
+        [TearDown]
+        public void Teardown()
+        {
+            _driver.Quit();
+        }
+    }
+}
